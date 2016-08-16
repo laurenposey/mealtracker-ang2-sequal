218b7092229ace2dcfa6ef0a754753b4ec66fc9e
Lauren Posey <mrslaurenposey@gmail.com>
2016-08-15 21:36:33 -0700

    display some html, working on getting meals to display in list

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+    <div class="container">
+      <h1>Meal Tracker</h1>
+      <meal-list [mealList]="meals">
+      </meal-list>
+    </div>
   `
 })
 export class AppComponent {
-
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+    new Meal("Bistro Box", 450, "Carrots, Snap Peas, Hummas, Cheese, Crackers"),
+    new Meal("Salad", 360, "Arugula, Spinach, Feta, Olives, Cucumber, Tomatoes, Pepperoncinis, and Tahini Dressing"),
+    new Meal("Mediterranean Parfait", 500, "Greek Yogurt, Cashews, Orange Slices, and Honey")
+  ];
+  }
 }
